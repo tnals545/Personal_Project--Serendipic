7a97ecbe90b138209a6859411b3060a4c1e8745e
sumin <tnals545@naver.com>
2023-04-12 20:44:16 +0900

    Fix: 페이지 새로고침 후 styled-components 적용 안됨 해결 - Next.js의 SSR 프레임워크 특성상 style이 적용되기 전에 렌더링되어 style이 적용되지 않는 문제 해결

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  compiler: {
+    styledComponents: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/services/background-image",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}?client_id=${process.env.NEXT_PUBLIC_API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
