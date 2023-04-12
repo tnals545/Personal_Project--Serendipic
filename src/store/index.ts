60cd28f88cce70bd14ac909deb05f0dcc546290a
sumin <tnals545@naver.com>
2023-04-12 21:11:19 +0900

    Feat: Redux setting - store - slice => keyword, alert

---
+++ src/store/index.ts
@@ -0,0 +1,43 @@
+import {
+  configureStore,
+  combineReducers,
+  AnyAction,
+  CombinedState,
+} from "@reduxjs/toolkit";
+import { createWrapper, HYDRATE } from "next-redux-wrapper";
+import { Reducer } from "@reduxjs/toolkit";
+
+import alertSlice, { type AlertState } from "@/store/slice/alertSlice";
+import keywordDataSlice, {
+  type KeywordDataState,
+} from "@/store/slice/keywordDataSlice";
+import { TestState, testApi } from "@/api/testApi";
+
+export interface RootState {
+  alert: AlertState;
+  keyword: KeywordDataState;
+  test: TestState;
+}
+
+const RootReducer = (
+  state: RootState,
+  action: AnyAction
+): CombinedState<RootState> => {
+  if (action.type === HYDRATE) return { ...state, ...action.payload };
+
+  const combinedReducer = combineReducers({
+    alert: alertSlice.reducer,
+    keyword: keywordDataSlice.reducer,
+    test: testApi.reducer,
+  });
+
+  return combinedReducer(state, action);
+};
+
+const makeStore = () =>
+  configureStore({
+    reducer: RootReducer as Reducer<CombinedState<RootState>, AnyAction>,
+    devTools: process.env.NODE_ENV === "development",
+  });
+
+export const wrapper = createWrapper(makeStore);
