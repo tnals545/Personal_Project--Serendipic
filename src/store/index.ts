636810a3cb88ce25dd5b034ad91c12283a2932d1
sumin <tnals545@naver.com>
2023-05-10 19:01:47 +0900

    Feat: RTK Query 적용 - 기존에 api를 불러올 때 사용하던 axios 코드를 RTK Query로 변경하여 적용

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,22 @@
+import { createWrapper, HYDRATE } from "next-redux-wrapper";
 import {
   configureStore,
   combineReducers,
   AnyAction,
   CombinedState,
 } from "@reduxjs/toolkit";
-import { createWrapper, HYDRATE } from "next-redux-wrapper";
 import { Reducer } from "@reduxjs/toolkit";
 
 import alertSlice, { type AlertState } from "@/store/slice/alertSlice";
 import keywordDataSlice, {
   type KeywordDataState,
 } from "@/store/slice/keywordDataSlice";
-import { TestState, testApi } from "@/api/testApi";
+import { SerendipicApiState, serendipicApi } from "@/api/serendipicApi";
 
 export interface RootState {
   alert: AlertState;
   keyword: KeywordDataState;
-  test: TestState;
+  serendipicApi: SerendipicApiState;
 }
 
 const RootReducer = (
@@ -28,7 +28,7 @@ const RootReducer = (
   const combinedReducer = combineReducers({
     alert: alertSlice.reducer,
     keyword: keywordDataSlice.reducer,
-    test: testApi.reducer,
+    serendipicApi: serendipicApi.reducer,
   });
 
   return combinedReducer(state, action);
@@ -38,6 +38,8 @@ const makeStore = () =>
   configureStore({
     reducer: RootReducer as Reducer<CombinedState<RootState>, AnyAction>,
     devTools: process.env.NODE_ENV === "development",
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(serendipicApi.middleware),
   });
 
 export const wrapper = createWrapper(makeStore);
