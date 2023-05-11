5bba037ae307920884a0991831cee539b6b8d40a
sumin <tnals545@naver.com>
2023-05-11 13:56:36 +0900

    Style: import 중복 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import { GlobalStyle } from "@/styles/global.style";
 import { wrapper } from "@/store";
 import "../../public/static/fonts/style.css";
-import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(pageProps);
